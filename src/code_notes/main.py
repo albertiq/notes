09a984d054642cd4ff735387d0a6f0f00612ce36
albertiq <alberttuygunov@gmail.com>
2024-05-14 15:10:41 +0500

    added main.py module

---
+++ src/code_notes/main.py
@@ -0,0 +1,13 @@
+import uvicorn
+
+from fastapi import FastAPI
+
+from config import settings
+from router import router as api_router
+
+app = FastAPI()
+
+app.include_router(api_router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=settings.web_port)
