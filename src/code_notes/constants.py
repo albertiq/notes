f48ea05b22731cf0937be41ed8f0a77db0e22848
albertiq <alberttuygunov@gmail.com>
2024-05-28 22:22:54 +0500

    added celery task creator note

--- src/code_notes/constants.py
+++ src/code_notes/constants.py
@@ -25,12 +25,34 @@ class Context:
         return self._task_signature.get_signature(args, signature)
 """
 CELERY_TASK_CREATOR = """
+class CeleryTaskCreator(ABC):
+    @abstractmethod
+    def get_payloads(self) -> list[dict]:
+        pass
 
+    def create_task(self, task_signature: Signature) -> Signature:
+        payloads = self.get_payloads()
+        for payload in payloads:
+            task_signature.kwargs = payload
+        return task_signature
+
+    def create_task_group(self, task_signature: Signature) -> group:
+        task_signatures = []
+        payloads = self.get_payloads()
+        for payload in payloads:
+            task_signature.kwargs = payload
+            task_sig = copy.copy(task_signature)
+            task_signatures.append(task_sig)
+        return group(task_signatures)
 """
 
 NOTES_DATA: list[dict] = [
     {
         "name": "Celery task signature strategy",
         "code_block": TASK_SIGNATURE_STRATEGY_PATTERN,
+    },
+    {
+        "name": "Celery task creator",
+        "code_block": CELERY_TASK_CREATOR,
     },
 ]
