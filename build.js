4c8c49d57cc4e2dd2fedf02138f2070835f2e060
albertiq <alberttuygunov@gmail.com>
2024-06-20 22:46:35 +0500

    deleted static + yaml actions

--- build.js
+++
@@ -1,9 +0,0 @@
-const fs = require('node-fs');
-
-fs.copy('src/static', 'build/static', (err) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log('Files copied successfully!');
-  }
-});
--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Copy static files to build directory
-cp -R src/static build/
-
-# Build FastAPI app
-uvicorn src.main:app --host 0.0.0.0 --port 8000 --reload
